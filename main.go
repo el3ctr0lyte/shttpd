b7b50f39837b12154fab2c1e939359179ae7cb3d
el3ctr0lyte <alanrogers4211@gmail.com>
2021-09-13 16:15:51 +0200

    Refactored the flag parsing.

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"os"
 )
-
-func printHelp() {
-	fmt.Println("USAGE: shttpd -a <host:port> -r <root directory>")
-	fmt.Println("       shttpd -a <host:port> -r <root directory> -k <keyfile> -c <certfile>")
-	fmt.Println("       shttpd -h")
-	fmt.Printf("\nThe root directory is where all your html files are kept.\nThe 'index.html' file is always the default index.\nIf no 'index.html' file is found, then a directory listing is served.\nThe key file and cert file is for TLS/SSL, and if specified, will enable TLS/SSL\nautomatically.\n")
-}
 
 func main() {
 	//Main variable used in the program and default values used if none are changed.
@@ -19,49 +11,30 @@ func main() {
 	var serveaddr string = "127.0.0.1:8080"
 	var keyfile string
 	var certfile string
-	helpasked, certspecified, keyspecified := false, false, false
 
-	//Check the parameters to change values if needed.
-	if len(os.Args) > 1 {
-		for i, v := range os.Args {
-			switch v {
-			case "-r":
-				rootserve = os.Args[i+1]
-			case "-a":
-				serveaddr = os.Args[i+1]
-			case "-c":
-				certfile = os.Args[i+1]
-				certspecified = true
-			case "-k":
-				keyfile = os.Args[i+1]
-				keyspecified = true
-			case "-h":
-				helpasked = true
-			}
-		}
-	}
+	var err error
 
-	//Was help asked?
-	if !helpasked {
+	flag.StringVar(&rootserve, "root", "./", "The root directory to serve from")
+	flag.StringVar(&serveaddr, "addr", "127.0.0.1:8080", "The address to bind to, eg 127.0.0.1:8080")
+	flag.StringVar(&keyfile, "key", "", "The SSL/TLS key file. SSL/TLS enabled if specified with cert")
+	flag.StringVar(&certfile, "cert", "", "The SSL/TLS cert file. SSL/TLS enabled if specified with key")
 
-		//if a cert and key was specified, make a TLS/SSL based connection. If not, make a normal connection.
-		var err error
-		if keyspecified && certspecified {
-			fmt.Println("Securely Serving", rootserve, "on", serveaddr)
-			err = ServeFilesSecure(rootserve, serveaddr, certfile, keyfile)
-		} else {
-			fmt.Println("Serving", rootserve, "on", serveaddr)
-			err = ServeFiles(rootserve, serveaddr)
-		}
+	flag.StringVar(&rootserve, "r", "./", "Shorthand for root")
+	flag.StringVar(&serveaddr, "a", "127.0.0.1:8080", "Shorthand for addr")
+	flag.StringVar(&keyfile, "k", "", "Shorthand for key")
+	flag.StringVar(&certfile, "c", "", "Shorthand for cert")
 
-		//Did something go wrong? Check for errors.
-		if err != nil {
-			log.Fatal(err)
-		}
+	flag.Parse()
+
+	if (keyfile != "") || (certfile != "") {
+		fmt.Println("Securely serving", rootserve, "on", serveaddr)
+		err = ServeFilesSecure(rootserve, serveaddr, certfile, keyfile)
 	} else {
-
-		//Prints the help
-		printHelp()
+		fmt.Println("Serving", rootserve, "on", serveaddr)
+		err = ServeFiles(rootserve, serveaddr)
 	}
 
+	if err != nil {
+		fmt.Println(err)
+	}
 }
